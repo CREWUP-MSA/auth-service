21934b33b7e84212aef0102df1ee39f35bb4a63b
h0ch1 <h0ch1@naver.com>
2024-10-23 19:12:21 +0900

    fix: swagger config (swagger 통합 설정)

--- src/main/java/com/example/authservice/config/SwaggerConfig.java
+++ src/main/java/com/example/authservice/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.example.authservice.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +15,16 @@ public class SwaggerConfig {
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
-			.info(new Info()
-				.title("Auth Service API")
-				.version("1.0")
-				.description("API Documentation for Auth Service")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
-			);
+			.components(new Components())
+			.addServersItem(new Server().url("/"))
+			.info(info());
+	}
+
+	private Info info() {
+		return new Info()
+			.title("Auth Service API")
+			.description("Auth Service API Description")
+			.version("1.0.0")
+			.license(new License().name("Apache 2.0").url("http://springdoc.org"));
 	}
 }
